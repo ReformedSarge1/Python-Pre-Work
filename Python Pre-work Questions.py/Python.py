7b63a248b390b5ca0bee766e64c6a35dd18a84d1
Carter Cook <raymanomen3014@gmail.com>
2022-09-18 19:10:19 -0700

    first commit

---
+++ Python Pre-work Questions.py/Python.py
@@ -0,0 +1,36 @@
+#Question 1
+#Write a funtion to print "hello_username!" is the input of the function.
+
+def hello_name(user_name):
+    print("hello_"+user_name)
+user_name=input('hello_USERNAME!')
+print(user_name)
+
+#Question 2
+#Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
+
+def first_odds():
+    numbers=int(input(1-100))
+mod = 1-100
+if mod > 0: print(7)
+else: print("1-100")
+
+
+#Question 3
+#Please write a python funtion, max_num_in_list
+def max_num_in_list(a_list):
+    a_list = [1, 2, 3, 4, 5, 6]
+    print(len()(a_list))
+
+#Question 4
+def is_leap_year(a_year):
+    a_year=4
+    leap_year=100
+    print(a_year, - leap_year)
+
+#Question 5
+def is_consecutive(a_list):
+    a_list=[2,3,4,5,6,7]
+    print(a_list)
+
+
